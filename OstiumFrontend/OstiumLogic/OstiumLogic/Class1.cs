a2f376454cfb8d8737bd630dc25d21c576bc897e
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-07-25 18:40:38 +0200

    step

--- OstiumFrontend/OstiumLogic/OstiumLogic/Class1.cs
+++ OstiumFrontend/OstiumLogic/OstiumLogic/Class1.cs
@@ -1,6 +1,13 @@
-﻿namespace OstiumLogic
+﻿using System;
+
+// Ostium
+namespace OstiumLogic
 {
     public class Class1
     {
+        public void PrintHelloWorld()
+        {
+            Console.Out.WriteLine("Hello world from OstiumLogic");
+        }
     }
 }
