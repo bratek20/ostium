58010309b0f47d3fef6ef7f6e4bf14c34b304c4e
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-07-28 08:50:45 +0200

    start game test passing

--- OstiumBackend/lib/src/main/kotlin/com/github/bratek20/ostium/gamesetup/impl/Logic.kt
+++ OstiumBackend/lib/src/main/kotlin/com/github/bratek20/ostium/gamesetup/impl/Logic.kt
@@ -6,6 +6,21 @@ import com.github.bratek20.ostium.gamecomponents.api.*
 
 class GameSetupApiLogic: GameSetupApi {
     override fun startGame(): Game {
-        TODO("Not yet implemented")
+        return Game.create(
+            hand = Hand.create(
+                cards = listOf(
+                    CreatureCard.create("HandCard1"),
+                    CreatureCard.create("HandCard2")
+                )
+            ),
+            table = Table.create(
+                creatureCard = CreatureCard.create("TableCreature"),
+                gateCard = GateCard.create(false),
+                gateDurabilityCard = GateDurabilityCard.create(
+                    myMarker = GateDurabilityMarker(15),
+                    opponentMarker = GateDurabilityMarker(15)
+                )
+            )
+        )
     }
 }
--- OstiumBackend/lib/src/test/kotlin/com/github/bratek20/ostium/gamesetup/tests/ImplTest.kt
+++ OstiumBackend/lib/src/test/kotlin/com/github/bratek20/ostium/gamesetup/tests/ImplTest.kt
@@ -1,11 +1,46 @@
 package com.github.bratek20.ostium.gamesetup.tests
 
+import com.github.bratek20.architecture.context.someContextBuilder
+import com.github.bratek20.ostium.gamesetup.api.GameSetupApi
+import com.github.bratek20.ostium.gamesetup.context.GameSetupImpl
+import com.github.bratek20.ostium.gamesetup.fixtures.assertGame
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class GameSetupImplTest {
     @Test
-    fun `TODO`() {
-        assertThat(true).withFailMessage("TODO").isEqualTo(false)
+    fun `start game`() {
+        val api = someContextBuilder()
+            .withModules(
+                GameSetupImpl()
+            )
+            .get(GameSetupApi::class.java)
+
+        val game = api.startGame()
+
+        assertGame(game) {
+            hand = {
+                cards = listOf(
+                    {
+                        name = "HandCard1"
+                    },
+                    {
+                        name = "HandCard2"
+                    }
+                )
+            }
+            table = {
+                creatureCard = {
+                    name = "TableCreature"
+                }
+                gateCard = {
+                    destroyed = false
+                }
+                gateDurabilityCard = {
+                    myMarker = 15
+                    opponentMarker = 15
+                }
+            }
+        }
     }
 }
