075a70d13053b3b09be15e9798be5e6ed72ff38b
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-07-28 08:42:33 +0200

    generation

---
+++ OstiumBackend/lib/src/main/kotlin/com/github/bratek20/ostium/gamesetup/impl/Logic.kt
@@ -0,0 +1,11 @@
+package com.github.bratek20.ostium.gamesetup.impl
+
+import com.github.bratek20.ostium.gamesetup.api.*
+
+import com.github.bratek20.ostium.gamecomponents.api.*
+
+class GameSetupApiLogic: GameSetupApi {
+    override fun startGame(): Game {
+        TODO("Not yet implemented")
+    }
+}
---
+++ OstiumBackend/lib/src/test/kotlin/com/github/bratek20/ostium/gamecomponents/tests/ImplTest.kt
@@ -0,0 +1,11 @@
+package com.github.bratek20.ostium.gamecomponents.tests
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class GameComponentsImplTest {
+    @Test
+    fun `TODO`() {
+        assertThat(true).withFailMessage("TODO").isEqualTo(false)
+    }
+}
