4f55a8f7d09df251cfc9bbd73719fab49ce5e11d
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-07-30 20:33:40 +0200

    jars and scripts for stachu

--- OstiumBackend/web-app-tester/build.gradle.kts
+++ OstiumBackend/web-app-tester/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.bratek20.internal.kotlin.library.conventions)
+
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 dependencies {
@@ -7,4 +9,15 @@ dependencies {
     testImplementation(testFixtures(project(":lib")))
 
     implementation(libs.bratek20.logs)
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    archiveClassifier.set("") // optional, removes the "-all" suffix from the JAR name
+    manifest {
+        attributes["Main-Class"] = "OstiumWebAppTesterKt" // Replace with your main class
+    }
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
--- OstiumBackend/web-app-tester/src/main/kotlin/OstiumWebAppTester.kt
+++ OstiumBackend/web-app-tester/src/main/kotlin/OstiumWebAppTester.kt
@@ -7,16 +7,20 @@ import com.github.bratek20.logs.context.Slf4jLogsImpl
 import com.github.bratek20.ostium.gamesetup.api.GameSetupApi
 import com.github.bratek20.ostium.gamesetup.context.GameSetupWebClient
 
-fun main() {
+fun main(args: Array<String>) {
+    var serverUrl = "http://localhost:8080"
+    if (args.isNotEmpty()) {
+        serverUrl = args[0]
+    }
+
     val c = SpringContextBuilder()
         .withModules(
             HttpClientImpl(),
             GameSetupWebClient(
                 config = HttpClientConfig.create(
-                    baseUrl = "http://localhost:8080"
+                    baseUrl = serverUrl
                 )
             ),
-
             Slf4jLogsImpl()
         )
         .build()
@@ -24,6 +28,9 @@ fun main() {
     val api = c.get(GameSetupApi::class.java)
     val logger = c.get(Logger::class.java)
 
+    logger.info("Using server URL = $serverUrl")
+
+    logger.info("Starting game...")
     val game = api.startGame()
     val gameAsJson = SerializationFactory.createSerializer()
         .serialize(game)
