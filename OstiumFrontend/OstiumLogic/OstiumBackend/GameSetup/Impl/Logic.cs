ae50390252e1af1cb9b408ced7d6df7dbc3709b0
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-08-03 08:23:56 +0200

    ostium backend migrated from kotlin with chatgpt

---
+++ OstiumFrontend/OstiumLogic/OstiumBackend/GameSetup/Impl/Logic.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameSetup.Impl
+{
+    public class GameSetupApiLogic : GameSetupApi
+    {
+        private List<CreatureCard> hand = new List<CreatureCard>
+        {
+            CreatureCard.Create(new CreatureCardId("Mouse1")),
+            CreatureCard.Create(new CreatureCardId("Mouse2"))
+        };
+
+        private CreatureCard? attackRow = null;
+        private CreatureCard? defenseRow = null;
+
+        public Game StartGame()
+        {
+            return ToApiGame();
+        }
+
+        public Game PlayCard(CreatureCardId cardId, RowType row)
+        {
+            var card = hand.First(c => c.GetId().Equals(cardId));
+            hand.Remove(card);
+
+            switch (row)
+            {
+                case RowType.ATTACK:
+                    attackRow = CreatureCard.Create(cardId);
+                    break;
+                case RowType.DEFENSE:
+                    defenseRow = CreatureCard.Create(cardId);
+                    break;
+            }
+
+            return ToApiGame();
+        }
+
+        public Game MoveCard(CreatureCardId cardId, RowType from, RowType to)
+        {
+            CreatureCard? card = from switch
+            {
+                RowType.ATTACK => attackRow,
+                RowType.DEFENSE => defenseRow,
+                _ => throw new InvalidOperationException($"No card in {from} row")
+            };
+
+            if (card == null)
+            {
+                throw new InvalidOperationException($"No card in {from} row");
+            }
+
+            switch (to)
+            {
+                case RowType.ATTACK:
+                    attackRow = card;
+                    break;
+                case RowType.DEFENSE:
+                    defenseRow = card;
+                    break;
+            }
+
+            switch (from)
+            {
+                case RowType.ATTACK:
+                    attackRow = null;
+                    break;
+                case RowType.DEFENSE:
+                    defenseRow = null;
+                    break;
+            }
+
+            return ToApiGame();
+        }
+
+        private Game ToApiGame()
+        {
+            return Game.Create(
+                Table.Create(
+                    GateDurabilityCard.Create(
+                        new GateDurabilityMarker(15),
+                        new GateDurabilityMarker(15)
+                    ),
+                    attackRow,
+                    defenseRow,
+                    GateCard.Create(false)
+                ),
+                Hand.Create(hand.ToList())
+            );
+        }
+    }
+}
