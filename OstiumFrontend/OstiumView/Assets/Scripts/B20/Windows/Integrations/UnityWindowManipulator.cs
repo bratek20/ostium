7deecb232fd6dba4d7b2ea8e1d98f1d8affa91bb
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-08-05 20:31:15 +0200

    unity integration started

---
+++ OstiumFrontend/OstiumView/Assets/Scripts/B20/Windows/Integrations/UnityWindowManipulator.cs
@@ -0,0 +1,12 @@
+using B20.Frontend.Windows.Api;
+
+namespace B20.Frontend.Windows.Integrations
+{
+    public class UnityWindowManipulator: WindowManipulator
+    {
+        public void SetVisible(WindowId id, bool visible)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- OstiumFrontend/OstiumView/Assets/Scripts/Ostium/GameManager.cs
+++ OstiumFrontend/OstiumView/Assets/Scripts/Ostium/GameManager.cs
@@ -1,3 +1,6 @@
+using B20.Frontend.Windows.Api;
+using B20.Frontend.Windows.Impl;
+using B20.Frontend.Windows.Integrations;
 using B20.Logic;
 using B20.View;
 using GameSetup.Api;
@@ -7,7 +10,7 @@ using UnityEngine;
 
 namespace Ostium.View
 {
-    public class GameManager : MonoBehaviour, GameStateListener
+    public class GameManager : MonoBehaviour
     {
         [SerializeField]
         private WindowView mainWindow;
@@ -16,40 +19,18 @@ namespace Ostium.View
         [SerializeField]
         private ButtonView playButton;
 
-        private GameState state;
+        private OstiumLogic logic;
 
         void Start()
         {
             mainWindow.Init(new MainWindow());
             gameWindow.Init(new GameWindow());
 
-            state = new GameState(this, mainWindow.Value);
+            var windowManipulator = new UnityWindowManipulator();
+            WindowManager windowManager = new WindowManagerLogic(windowManipulator);
+            logic = new OstiumLogic(new WindowManagerLogic(windowManipulator));
 
-            playButton.Init(new PlayButton(state, gameWindow.Value as GameWindow));
-
-            UpdateView();
-        }
-
-        public void OnStateChanged()
-        {
-            UpdateView();
-        }
-
-        private void UpdateView()
-        {
-            if (state.CurrentWindow == mainWindow.Value)
-            {
-                mainWindow.SetActive(true);
-                gameWindow.SetActive(false);
-            }
-            if (state.CurrentWindow == gameWindow.Value)
-            {
-                mainWindow.SetActive(false);
-                gameWindow.SetActive(true);
-
-                GameSetupApi api = new GameSetupApiLogic();
-                gameWindow.Init2(api.StartGame());
-            }
+            playButton.Init(new PlayButton(windowManager));
         }
     }
 }
