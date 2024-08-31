ac693bdc266aa46cce6446899a41acd0e4a0de94
Bartosz Rudzki (Legion) <bartosz.rudzki@tensquaregames.com>
2024-08-31 16:52:14 +0200

    green

--- Frontend/Logic/B20/Infrastructure/HttpClient/Impl/Logic.cs
+++ Frontend/Logic/B20/Infrastructure/HttpClient/Impl/Logic.cs
@@ -1,12 +1,38 @@
-﻿using HttpClient.Api;
+﻿using B20.Ext;
+using HttpClient.Api;
 
 namespace HttpClient.Impl
 {
+    class HttpResponseLogic: HttpResponse
+    {
+        public int getStatusCode()
+        {
+            return 200;
+        }
+
+        public Optional<object> getBody<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class HttpClientLogic : Api.HttpClient
+    {
+        public HttpResponse Get(string path)
+        {
+            return new HttpResponseLogic();
+        }
+
+        public HttpResponse Post(string path, Optional<object> body)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
     public class HttpClientFactoryLogic: HttpClientFactory
     {
         public Api.HttpClient Create(HttpClientConfig config)
         {
-            throw new System.NotImplementedException();
+            return new HttpClientLogic();
         }
     }
 }
