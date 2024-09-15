6f827a109e50247872d5071b17323ea4b507b4cc
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-09-15 13:23:33 +0200

    tweaks

--- Backend/bash/server/buildWebApp.sh
+++ Backend/bash/server/buildWebApp.sh
@@ -5,5 +5,4 @@ cd ..
 ./gradlew clean build
 
 cp web-app/build/libs/web-app.jar bash/server/
-cp web-app-tester/build/libs/web-app-tester.jar bash/server/
 
--- Backend/bash/server/runWebAppTester.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SERVER_URL=$1
-
-java -jar web-app-tester.jar $SERVER_URL
-
