3f0405e9d9a6c8fc52fb417bef9ca231ea838bb2
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-09-20 17:17:05 +0200

    fixes

--- Frontend/Logic/B20/Architecture/Contexts/Api/Exceptions.cs
+++ Frontend/Logic/B20/Architecture/Contexts/Api/Exceptions.cs
@@ -2,6 +2,13 @@ using B20.Architecture.Exceptions;
 
 namespace B20.Architecture.Contexts.Api
 {
+    public class ClassNotFoundInContextException : ApiException
+    {
+        public ClassNotFoundInContextException(string message) : base(message)
+        {
+        }
+    }
+    
     public class DependentClassNotFoundInContextException : ApiException
     {
         public DependentClassNotFoundInContextException(string message) : base(message)
